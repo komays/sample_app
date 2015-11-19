7415021dbb53702084aaa4ad5714e664d158bbd8
komays <komays@umich.edu>
2015-11-19 14:34:40 -0600

    try to implement bookmarklet

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,16 @@ module SampleApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.middleware.use Rack::Cors do
+     allow do
+      origins '*'
+      resource '*',
+        :headers => :any,
+        :methods => [:get, :put, :delete]
+      end
+    end
+
+    
   end
 end
